ffdf7bcd6f3849a6af11718df9080e7fc81bd343
laba2 <laba2>
2026-06-07 16:10:47 +0300

    add top-N attackers by IP

--- parser.py
+++ parser.py
@@ -1,8 +1,17 @@
-with open("auth.log", "r") as file:
+import operator
+def serch_failed(poisk, n):
     schet=0
-    lines = file.readlines()
-    for i in lines:
+    ip_count={}
+    for i in poisk:
         if "Failed" in i:
             schet+=1
-            print(i)
-    print(schet)
+            ip=i.split()[10]
+            if ip in ip_count:
+                ip_count[ip]+=1
+            else:
+                ip_count[ip]=1
+    return (sorted(ip_count.items(),key= operator.itemgetter(1),reverse=True))[:n]
+with open("auth.log", "r") as poisk:
+    n = int(input("Выберете количество смаых повторяющихся айпи адресов: "))
+    poisk = poisk.readlines()
+    print(serch_failed(poisk,n))
